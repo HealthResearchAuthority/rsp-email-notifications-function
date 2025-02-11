73a3e7491afbb42309805ce17e024ce6fec25f33
Shahzad Hassan <shahzad.hassan@paconsulting.com>
2025-02-11 15:51:53 +0000

    Created an empty branch with README.md

---
+++ src/Application/Constants/NotifyServiceExceptions.cs
@@ -0,0 +1,8 @@
+namespace Rsp.NotifyFunction.Application.Constants;
+
+public struct NotifyServiceExceptions
+{
+    public const string GovUKNotifyClientException = "GOV_UK_NOTIFY_CLIENT_EXCEPTION";
+    public const string ServerError = "SERVER_ERROR";
+    public const string FunctionError = "FUNCTION_ERROR";
+}
