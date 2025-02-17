5a79412a3cdb706f534f6dcdc17a04193e3675c9
Amari Hussey Lawal <amari.lawal@paconsulting.com>
2025-02-17 13:36:15 +0000

    RSP-2541: Create Notification Azure Function - Changed folder structure and ignored serviceDependencies.local.json - Included .editorconfig file - Included .gitignore file - Included README.md file - Updated azure-pilines.yaml - Addressed PR comments

--- src/Functions/NotifyFunction.cs
+++ src/Functions/NotifyFunction.cs
@@ -1,6 +1,5 @@
 using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Rsp.Logging.Extensions;
@@ -9,38 +8,35 @@ using Rsp.NotifyFunction.Application.Models;
 
 namespace Rsp.NotifyFunction.Functions;
 
-public class NotifyFunction
+public class NotifyFunction(ILogger<NotifyFunction> logger, INotifyService rspNotifyService)
 {
-    private readonly ILogger<NotifyFunction> _logger;
-    private readonly INotifyService _rspNotifyService;
-
-    public NotifyFunction(IConfiguration configuration, ILogger<NotifyFunction> logger, INotifyService rspNotifyService)
-    {
-        _logger = logger;
-        _rspNotifyService = rspNotifyService;
-    }
-
     // function that listens to the azure service bus queue for new messages and is triggered when a new message is added to the queue
     [Function(nameof(NotifyFunction))]
-    public async Task Run(
+    public async Task Run
+    (
         [ServiceBusTrigger("%QueueName%", Connection = "EmailNotificationsConnection")]
-            ServiceBusReceivedMessage message,
-        ServiceBusMessageActions messageActions)
+        ServiceBusReceivedMessage message,
+        ServiceBusMessageActions messageActions
+    )
     {
         // parse incoming message into the EmailNotificationMessage object
         var messageBody = message.Body.ToString();
-        _logger.LogAsInformation($"Sending email...");
+
+        logger.LogAsInformation("Sending email...");
+
         var messageJson = JsonConvert.DeserializeObject<EmailNotificationMessage>(messageBody);
 
         // send the email via the Gov UK notification service
-        var response = await _rspNotifyService.SendEmail(messageJson);
+        var response = await rspNotifyService.SendEmail(messageJson);
+
         if (response != null)
         {
             // Complete the message which removes the message from the queue since it has been proccessed
             await messageActions.CompleteMessageAsync(message);
         }
 
-        var parameters = $"Email Name: {messageJson.EventName}, Email Address: {messageJson.RecipientAddress}, Email TemplateId: {messageJson.EmailTemplateId}";
-        _logger.LogAsInformation(parameters: parameters, "Email successfully sent.");
+        var parameters = $"EventName: {messageJson.EventName}, RecipientAddress: {messageJson.RecipientAddress}, TemplateId: {messageJson.EmailTemplateId}";
+
+        logger.LogAsInformation(parameters: parameters, "Email successfully sent.");
     }
 }
--- src/Startup/Configuration/Services/ServiceConfiguration.cs
+++ src/Startup/Configuration/Services/ServiceConfiguration.cs
@@ -6,7 +6,7 @@ using Rsp.NotifyFunction.Application.Configuration;
 using Rsp.NotifyFunction.Application.Contracts;
 using Rsp.NotifyFunction.Services;
 
-namespace Rsp.NotifyFunction.Startup.Configuration;
+namespace Rsp.NotifyFunction.Startup.Configuration.Services;
 
 public static class ServicesConfiguration
 {
@@ -15,10 +15,17 @@ public static class ServicesConfiguration
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
         services.AddTransient<INotifyService, NotifyService>();
+<<<<<<<< HEAD:src/Startup/Configuration/ServiceConfiguration.cs
         services.AddTransient<IAsyncNotificationClient, NotificationClient>(sp =>
         {
             return new NotificationClient(appSettings.GovNotifyApiKey);
         });
+========
+        services.AddTransient<IAsyncNotificationClient, NotificationClient>
+        (_ =>
+            new NotificationClient(appSettings.GovNotifyApiKey)
+        );
+>>>>>>>> 2af55c7 (RSP-2541: Create Notification Azure Function):src/Startup/Configuration/Services/ServiceConfiguration.cs
 
         return services;
     }
